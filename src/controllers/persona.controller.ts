d26d5ff54bc222c0ad1758e79ac2ac8b2a18e7be
EdwinG24 <84823791+EdwinG241993@users.noreply.github.com>
2021-11-11 01:15:28 -0500

    version 1.1.0 de la aplicacion pedidos

--- src/controllers/persona.controller.ts
+++ src/controllers/persona.controller.ts
@@ -1,29 +1,28 @@
+import {service} from '@loopback/core';
 import {
   Count,
   CountSchema,
   Filter,
   FilterExcludingWhere,
   repository,
-  Where,
+  Where
 } from '@loopback/repository';
 import {
-  post,
-  param,
-  get,
-  getModelSchemaRef,
-  patch,
-  put,
-  del,
-  requestBody,
-  response,
+  del, get,
+  getModelSchemaRef, param, patch, post, put, requestBody,
+  response
 } from '@loopback/rest';
 import {Persona} from '../models';
 import {PersonaRepository} from '../repositories';
+import {AutenticacionService} from '../services';
+const fetch = require("node-fetch");
 
 export class PersonaController {
   constructor(
     @repository(PersonaRepository)
     public personaRepository : PersonaRepository,
+    @service(AutenticacionService)
+    public servicioAutenticacion : AutenticacionService,
   ) {}
 
   @post('/personas')
@@ -44,8 +43,23 @@ export class PersonaController {
     })
     persona: Omit<Persona, 'id'>,
   ): Promise<Persona> {
-    return this.personaRepository.create(persona);
+
+    let clave = this.servicioAutenticacion.GenerarClave();
+    let claveCifrada = this.servicioAutenticacion.CifrarClave(clave);
+    persona.clave = claveCifrada;
+
+    let p = await this.personaRepository.create(persona);
+    let destino = persona.correo;
+    let asunto = "Registro app pedidos";
+    let contenido = `Hola ${persona.nombres}, su nombre de usuario es: ${persona.correo}, y su contraseña asignada es ${clave}`;
+    fetch(`http://127.0.0.1:5000/email?correo_destino=${destino}&asunto=${asunto}&contenido=${contenido}`)
+    .then((data:any)=>{
+      console.log(data);
+    })
+
+    return p;
   }
+
 
   @get('/personas/count')
   @response(200, {
